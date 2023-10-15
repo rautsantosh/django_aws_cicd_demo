026cd0a8619e7d48bc34762cae31e197763e77a9
Santosh <rautsantosh20@gmail.com>
2023-10-15 13:18:36 +0530

    added server ip to allowed host

--- notspam/settings.py
+++ notspam/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y0=a&r()1q&)vq6s2c44z4(9g3y7#@0jdg$=km7aoia-n1(^v$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.51.162.167']
 
 
 # Application definition
