4dc1efacb8b8b58ef5319cc17351239c69bdac08
Santosh <rautsantosh20@gmail.com>
2023-10-15 10:39:12 +0530

    custom login implemented

---
+++ scripts/nginx.sh
@@ -0,0 +1,13 @@
+
+#!/usr/bin/bash
+
+sudo systemctl daemon-reload
+sudo rm -f /etc/nginx/sites-enabled/default
+
+sudo cp /home/ubuntu/django_custom_user_model/nginx/nginx.conf /etc/nginx/sites-available/notspam
+sudo ln -s /etc/nginx/sites-available/notspam /etc/nginx/sites-enabled/
+#sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
+#sudo nginx -t
+sudo gpasswd -a www-data ubuntu
+sudo systemctl restart nginx
+
