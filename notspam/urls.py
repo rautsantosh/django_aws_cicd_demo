8633add26e3464f87491ee441ace487ceedf2645
santoshraut <santoshr@fecdirect.co>
2023-10-30 17:38:28 +0530

    serve static files in productions

--- notspam/urls.py
+++ notspam/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.urls import re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve 
 from django.conf import settings
 
 # from servers import views as server_views
@@ -30,4 +32,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += (
+# re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+)
