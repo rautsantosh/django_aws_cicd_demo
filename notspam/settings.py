291ac047489ddb9ff7dc68f962203f4cc106a68b
santoshraut <santoshr@fecdirect.co>
2023-10-30 22:49:38 +0530

    serve static files in productions

--- notspam/settings.py
+++ notspam/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "public")]
-STATICFILES_DIRS = [BASE_DIR / 'public']
+STATICFILES_DIRS = [BASE_DIR / 'public', BASE_DIR / 'staticfiles']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- notspam/urls.py
+++ notspam/urls.py
@@ -32,9 +32,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-urlpatterns += (
-# re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-)
+# urlpatterns += (
+# # re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+# re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+# )
