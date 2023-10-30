d3bd3044c42bc444840708d2568b76fb93b1e4eb
santoshraut <santoshr@fecdirect.co>
2023-10-30 21:56:53 +0530

    serve static files in productions

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash 
 
 sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/django_aws_cicd_demo/notspam/settings.py
-source /home/ubuntu/env/bin/activate
+# source /home/ubuntu/env/bin/activate
 cd /home/ubuntu/django_aws_cicd_demo/ && python3 manage.py migrate
 cd /home/ubuntu/django_aws_cicd_demo/ && python3 manage.py makemigrations
-cd /home/ubuntu/django_aws_cicd_demo/ && sudo python3 manage.py collectstatic
+cd /home/ubuntu/django_aws_cicd_demo/ && python3 manage.py collectstatic
 sudo chown ubuntu:ubuntu /home/ubuntu/django_aws_cicd_demo
 sudo chown ubuntu:ubuntu /home/ubuntu/django_aws_cicd_demo/public
 sudo service gunicorn restart
